f05a029d4a2f034b40ce8ecc7105601219d9e111
Eric Vo <vo.eric@gmail.com>
2017-12-20 17:19:35 -0500

    add redux promise, react-redux react redux and others

---
+++ src/containers/weather_list.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../compoments/google_map';
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import ReduxPromise from 'redux-promise';
 
 import SearchBar from './containers/search_bar';
 
 import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleWare(ReduxPromise)
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('root'));
