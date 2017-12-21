4c0c022b4fdee0102c3c03b9ca7b5912df17d109
Eric Vo <vo.eric@gmail.com>
2017-12-21 17:24:14 -0500

    add redux and weatherlist code

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,3 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
 import GoogleMap from '../compoments/google_map';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressues = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+
+    return(
+      <tr key={name}>
+        <td><GoogleMap lat={lat} lon={lon} /></td>
+        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+      </table>
+
+      <tbody>
+        {this.props.weather.map(this.renderWeather)}
+      </tbody>
+    </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-
-import SearchBar from './containers/search_bar';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleWare(ReduxPromise)
+const store = createStore(reducers, applyMiddleWare(ReduxPromise));
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, document.getElementById('root'));
 });
