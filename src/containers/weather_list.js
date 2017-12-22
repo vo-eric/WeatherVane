3b3bcb79b0ee7136e9369cbac5034361a2065982
Eric Vo <vo.eric@gmail.com>
2017-12-21 21:54:44 -0500

    fixed typos

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,7 +7,7 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    const pressues = cityData.list.map(weather => weather.main.pressure);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
     const { lon, lat } = cityData.city.coord;
 
